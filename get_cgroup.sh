ac42da8f394427ef157761c4bc04c3f8164ef533
aihua53 <aihua53@163.com>
2022-06-16 11:11:09 +0800

    update get_cgroup.sh

--- get_cgroup.sh
+++ get_cgroup.sh
@@ -1,35 +1,47 @@
-cd /sdcard
-mkdir cgroup
-cd ..
+# cd /sdcard
+# mkdir cgroup
+# cd ..
+# mkdir $1
+# cd ..
 
-echo background >> /sdcard/cgroup/cpuset.txt
-cat /dev/cpuset/background/cpus >> /sdcard/cgroup/cpuset.txt
-echo top-app >> /sdcard/cgroup/cpuset.txt
-cat /dev/cpuset/top-app/cpus >> /sdcard/cgroup/cpuset.txt
-echo foreground >> /sdcard/cgroup/cpuset.txt
-cat /dev/cpuset/foreground/cpus >> /sdcard/cgroup/cpuset.txt
-echo vip-app >> /sdcard/cgroup/cpuset.txt
-cat /dev/cpuset/vip-app/cpus >> /sdcard/cgroup/cpuset.txt
-echo system-background >> /sdcard/cgroup/cpuset.txt
-cat /dev/cpuset/system-background/cpus >> /sdcard/cgroup/cpuset.txt
-echo camera-daemon >> /sdcard/cgroup/cpuset.txt
-cat /dev/cpuset/camera-daemon/cpus >> /sdcard/cgroup/cpuset.txt
+echo "help info"
+echo "sh /system/bin/get_cgroup.sh name"
+echo "file saved in /sdcard/cgroup/name"
 
-ps -eT > /sdcard/cgroup/thread.txt
-cat /dev/cpuset/background/cgroup.procs > /sdcard/cgroup/bg_procs.txt
-cat /dev/cpuset/background/tasks > /sdcard/cgroup/bg_tasks.txt
+path="/sdcard/cgroup/"$1
+mkdir -p $path
 
-cat /dev/cpuset/top-app/cgroup.procs > /sdcard/cgroup/top_procs.txt
-cat /dev/cpuset/top-app/tasks > /sdcard/cgroup/top_tasks.txt
+path_cpuset=$path/cpuset.txt
+echo background >> $path_cpuset
+cat /dev/cpuset/background/cpus >> $path_cpuset
+echo top-app >> $path_cpuset
+cat /dev/cpuset/top-app/cpus >> $path_cpuset
+echo foreground >> $path_cpuset
+cat /dev/cpuset/foreground/cpus >> $path_cpuset
+echo vip-app >> $path_cpuset
+cat /dev/cpuset/vip-app/cpus >> $path_cpuset
+echo system-background >> $path_cpuset
+cat /dev/cpuset/system-background/cpus >> $path_cpuset
+echo camera-daemon >> $path_cpuset
+cat /dev/cpuset/camera-daemon/cpus >> $path_cpuset
 
-cat /dev/cpuset/foreground/cgroup.procs > /sdcard/cgroup/fg_procs.txt
-cat /dev/cpuset/foreground/tasks > /sdcard/cgroup/fg_tasks.txt
+ps -eT > $path/thread.txt
+ps -e > $path/process.txt
 
-cat /dev/cpuset/vip-app/cgroup.procs > /sdcard/cgroup/vip_procs.txt
-cat /dev/cpuset/vip-app/tasks > /sdcard/cgroup/vip_tasks.txt
+cat /dev/cpuset/background/cgroup.procs > $path/bg_procs.txt
+cat /dev/cpuset/background/tasks > $path/bg_tasks.txt
 
-cat /dev/cpuset/system-background/cgroup.procs > /sdcard/cgroup/sys_bg_procs.txt
-cat /dev/cpuset/system-background/tasks  > /sdcard/cgroup/sys_bg_tasks.txt
+cat /dev/cpuset/top-app/cgroup.procs > $path/top_procs.txt
+cat /dev/cpuset/top-app/tasks > $path/top_tasks.txt
 
-cat /dev/cpuset/camera-daemon/cgroup.procs > /sdcard/cgroup/camera-daemon_procs.txt
-cat /dev/cpuset/camera-daemon/tasks  > /sdcard/cgroup/camera-daemon_tasks.txt
+cat /dev/cpuset/foreground/cgroup.procs > $path/fg_procs.txt
+cat /dev/cpuset/foreground/tasks > $path/fg_tasks.txt
+
+cat /dev/cpuset/vip-app/cgroup.procs > $path/vip_procs.txt
+cat /dev/cpuset/vip-app/tasks > $path/vip_tasks.txt
+
+cat /dev/cpuset/system-background/cgroup.procs > $path/sys_bg_procs.txt
+cat /dev/cpuset/system-background/tasks  > $path/sys_bg_tasks.txt
+
+cat /dev/cpuset/camera-daemon/cgroup.procs > $path/camera-daemon_procs.txt
+cat /dev/cpuset/camera-daemon/tasks  > $path/camera-daemon_tasks.txt
