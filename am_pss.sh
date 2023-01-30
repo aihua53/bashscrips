d31e6531784aeeaa35608dc9d1530da393ba368e
aihua53 <aihua53@163.com>
2023-01-30 14:34:58 +0800

    update am_pss.sh

--- am_pss.sh
+++ am_pss.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-grep -rsn "am_pss" logcat.log > tmp.txt
-rm result.csv
-cat tmp.txt | while read line
-do
+grep -rsn "am_pss" $1 > tmp.txt
+# rm result.csv
+# cat tmp.txt | while read line;
+# do
+#     array=(${line//,/ })
+#     echo ${array[1]}, ${array[9]}, ${array[10]} >> result.csv
+# done
+
+cat tmp.txt | while read line;do
     array=(${line//,/ })
-    echo ${array[1]}, ${array[9]}, ${array[10]} >> result.csv
+    if [ ${array[10]} -gt 1000000000 ];
+    then
+        echo ${array[1]}, ${array[9]}, ${array[10]}
+    fi
 done
+
 
 
